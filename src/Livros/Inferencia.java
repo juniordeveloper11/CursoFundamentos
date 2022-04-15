0cee11d89817676bc7537643677b3c5439c2f75c
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2022-04-15 00:10:44 -0300

    JAVA

---
+++ src/Livros/Inferencia.java
@@ -0,0 +1,12 @@
+package Livros;
+
+public class Inferencia {
+
+    public static void main(String[] args) {
+        var   b =  4.5;
+        System.out.println(b);
+    }
+}
+
+
+
---
+++ src/TiposPrimitivos.java
@@ -0,0 +1,32 @@
+public class TiposPrimitivos {
+    public static void main(String[] args) {
+        // informações de funcionario
+        // tipos numericos
+        byte AnosdeEmpresa = 34;
+        short NumerosDeVoos = 345;
+        int id = 456789;
+        long PontoAcumulados = 3_456_345_55L;
+        // TIPOS NUMERICOS REAIS
+        float Salario = 8_234.57F;
+        double VendasAcumuladas = 2_345_345.67;
+        // Tipo Bouleano
+        boolean EstadoDeFerias = false; // true
+        // tipos de caractere
+        char status = '?';
+        System.out.println( AnosdeEmpresa*365);
+        System.out.println( Salario);
+        System.out.println(EstadoDeFerias);
+        System.out.println(VendasAcumuladas);
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
